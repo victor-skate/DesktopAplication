b50132917c7d277160f7b8643acb4940b1cd612f
victor moraes <victorhardflipmoraes@outlook.com>
2021-09-09 16:10:22 -0300

    update department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listner.DataChangeListner;
@@ -18,64 +20,84 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
-public class DepartmentFormController implements Initializable{
-	
+//CLASSE CONTROLADORA DO FORMULARIO DE INSCRIÇÃO DE UM NOVO DEPARTAMENTO
+public class DepartmentFormController implements Initializable {
+
 	private Department entity;
 	private DepartmentService service;
-	
-	//LISTA DE OUVINTES DE ALTERAÇÃO DE DADOS
+
+	// LISTA DE OUVINTES DE ALTERAÇÃO DE DADOS
 	private List<DataChangeListner> dataChangeListners = new ArrayList<>();
-	
+
 	@FXML
 	private TextField txtId;
-	
+
 	@FXML
 	private TextField txtName;
-	
+
 	@FXML
 	private Label labelErrorName;
-	
+
 	@FXML
 	private Button btSave;
-	
-	@FXML 
+
+	@FXML
 	private Button btCancel;
-	
+
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
-		if(entity == null) {
+		if (entity == null) {
 			throw new IllegalStateException("Entity was null");
 		}
-		if(service== null) {
+		if (service == null) {
 			throw new IllegalStateException("Service was null");
 		}
-		
+
 		try {
-		entity = getFormData();
-		service.saveOrUpdate(entity);
-		//NOTIFICAR LISTA DE ALTERAÇÃO DE DADOS
-		notifyDataChangeListeners();
-		Utils.currentStage(event).close();
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			// NOTIFICAR LISTA DE ALTERAÇÃO DE DADOS
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
 		}
-		catch(DbException e) {
-			Alerts.showAlert("Error saving object", null,e.getMessage(), AlertType.ERROR);
+		//SE O CAMPO NAME ESTIVER VAZIO, É LANÇADA EXCESSÃO
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
 		}
-		
+
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
-	//METODO PARA INSCREVER OUVINTE DE ALTERAÇÃO DE DADOS Á LISTA DE OUVINTES
+
+	}
+
+	// METODO RESPONSAVEL POR CHAMAR O METODO updateTableView
 	private void notifyDataChangeListeners() {
-		for(DataChangeListner listener: dataChangeListners) {
+		for (DataChangeListner listener : dataChangeListners) {
 			listener.onDataChange();
 		}
 	}
 
-	//METODO QUE PEGA OS DADOS DO FORMULARIO E RETORNA UM NOVO OBJETO Department
+	// METODO QUE PEGA OS DADOS DO FORMULARIO E RETORNA UM NOVO OBJETO Department
 	private Department getFormData() {
 		Department obj = new Department();
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+		ValidationException exception = new ValidationException("Validation error");
+
+		// SE O CAMPO NAME ESTIVER VAZIO, É LANÇADA UMA EXCESSÃO
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty");
+		}
 		obj.setName(txtName.getText());
+
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
 		return obj;
 	}
 
@@ -83,7 +105,7 @@ public class DepartmentFormController implements Initializable{
 	public void onBtCancelAction(ActionEvent event) {
 		Utils.currentStage(event).close();
 	}
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		initializeNodes();
@@ -93,17 +115,20 @@ public class DepartmentFormController implements Initializable{
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
 	}
-	 //ADICIONA NAS CAIXAS DE TEXTO DO FORMULÁRIO OS DADOS DO OBJETO INSTANCIADO.
+
+	// ADICIONA NAS CAIXAS DE TEXTO DO FORMULÁRIO OS DADOS DO OBJETO INSTANCIADO.
 	public void updateFormData() {
-		if(entity == null) {
+		if (entity == null) {
 			throw new IllegalStateException("Entity was null");
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
 	}
-	
-	/*O OBJ INSTANCIADO SERVE TANTO PARA ADD UM NOVO OBJ QUANTO PARA ATUALIZAR OS DADOS
-	 DE UM DEP EXISTENTE*/
+
+	/*
+	 * O OBJ INSTANCIADO SERVE TANTO PARA ADD UM NOVO OBJ QUANTO PARA ATUALIZAR OS
+	 * DADOS DE UM DEP EXISTENTE
+	 */
 	public void setEntity(Department entity) {
 		this.entity = entity;
 	}
@@ -114,5 +139,14 @@ public class DepartmentFormController implements Initializable{
 
 	public void subscribeDataChangeListner(DataChangeListner listener) {
 		dataChangeListners.add(listener);
+	}
+
+	//RECEBE UMA COLEÇÃO DE CHAVE E VALOR, ADICIONA AO fields APENAS A CHAVE DA COLEÇÃO errors	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
 	}
 }
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -9,6 +9,7 @@ import application.Main;
 import gui.listner.DataChangeListner;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -27,15 +29,17 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable,DataChangeListner {
+public class DepartmentListController implements Initializable, DataChangeListner {
 
 	private DepartmentService service;
 
 	@FXML
 	private TableView<Department> tableViewDepartment;
 
-	/*PRIMEIRO ATRIBUTO ESPECIFICA O TIPO DE ENTIDADE, O SEGUNDO INDICA A COLUNA DA
-	 * TABELA QUE NO CASO É O ID*/
+	/*
+	 * PRIMEIRO ATRIBUTO ESPECIFICA O TIPO DE ENTIDADE, O SEGUNDO INDICA A COLUNA DA
+	 * TABELA QUE NO CASO É O ID
+	 */
 	@FXML
 	private TableColumn<Department, Integer> tableColumnId;
 
@@ -43,29 +47,37 @@ public class DepartmentListController implements Initializable,DataChangeListner
 	private TableColumn<Department, String> tableColumnName;
 
 	@FXML
+	private TableColumn<Department, Department> tableColumnEDIT;
+
+	@FXML
 	private Button btNew;
 
-	@FXML//
+	@FXML //
 	private ObservableList<Department> obsList;
 
-	@FXML/*ActionEvent FORNECE UMA REFERÊNCIA PARA O CONTROLE QUE RECEBEU O EVENTO,
-	A PARTIR DESSE EVENT TEREI CONDIÇÃO DE ACESSAR O STAGE QUE GEROU O NOVO PALCO
- 	UTILIZANDO A FUNÇÃO DA CLASSE Utils*/
+	@FXML /*
+			 * ActionEvent FORNECE UMA REFERÊNCIA PARA O CONTROLE QUE RECEBEU O EVENTO, A
+			 * PARTIR DESSE EVENT TEREI CONDIÇÃO DE ACESSAR O STAGE QUE GEROU O NOVO PALCO
+			 * UTILIZANDO A FUNÇÃO DA CLASSE Utils
+			 */
 	public void onBtNewAction(ActionEvent event) {
-		Stage parentStage = Utils.currentStage(event);	
-		/*CHAMA O METODO QUE CRIA UM NOVO PALCO(STAGE) PASSANDO O CAMINHO DA VIEW
-		 * E O STAGE QUE RECEBEU O EVENTO PARA GERAR UM NOVO STAGE*/
+		Stage parentStage = Utils.currentStage(event);
+		/*
+		 * CHAMA O METODO QUE CRIA UM NOVO PALCO(STAGE) PASSANDO O CAMINHO DA VIEW E O
+		 * STAGE QUE RECEBEU O EVENTO PARA GERAR UM NOVO STAGE
+		 */
 		Department obj = new Department();
-		//OBJ É PASSADO COMO PARAMETRO PARA RECEBER OS DADOS DO FORMULARIO, SEJA PARA ATUALIZAR OU
-		//ADICIONAR UM NOVO DEPARTAMENTO
-		createDialogForm(obj,"/gui/DepartmentForm.fxml",parentStage);
+		// OBJ É PASSADO COMO PARAMETRO PARA RECEBER OS DADOS DO FORMULARIO, SEJA PARA
+		// ATUALIZAR OU
+		// ADICIONAR UM NOVO DEPARTAMENTO
+		createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
 
-	//INICIALIZA APROPRIADAMENTE O COMPORTAMENTO DAS COLUNAS DA TABELA
+	// INICIALIZA APROPRIADAMENTE O COMPORTAMENTO DAS COLUNAS DA TABELA
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -77,65 +89,94 @@ public class DepartmentListController implements Initializable,DataChangeListner
 		// stage.heightProperty
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
-	
-	
-	/*METODO RESPONSAVEL POR ACESSAR O SERVIÇO, CARREGAR OS DEPARTAMENTOS E JOGAR OS DEPARTAMENT0S
-	 * NA obsList, APÓS ISSO É SÓ ASSOCIAR A obsList AO tableView E OS DEPARTAMENTOS APARECERÃO NA
-	 * TELA */
+
+	/*
+	 * METODO RESPONSAVEL POR ACESSAR O SERVIÇO, CARREGAR OS DEPARTAMENTOS E JOGAR
+	 * OS DEPARTAMENT0S NA obsList, APÓS ISSO É SÓ ASSOCIAR A obsList AO tableView E
+	 * OS DEPARTAMENTOS APARECERÃO NA TELA
+	 */
 	public void updateTableView() {
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Service was null");
 		}
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+		initEditButtons();//METODO ACRESCENTE UM NOVO BOTÃO EDIT EM CADA LINHA DA TABELA
+		//QUANDO CLICADO NO BOTÃO, É GERADO UM FORMULARIO DE EDIÇÃO
 	}
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		initializeNodes();
 	}
-	
-	/*É NECESSARIO PASSAR COMO PARÂMETRO UMA REFERÊNCIA PARA O STAGE DA JANELA QUE CRIOU A JANELA
-	 *DE DIALOGO*/
+
+	/*
+	 * É NECESSARIO PASSAR COMO PARÂMETRO UMA REFERÊNCIA PARA O STAGE DA JANELA QUE
+	 * CRIOU A JANELA DE DIALOGO
+	 */
 	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
 		try {
-			 FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			 Pane pane = loader.load();//INSTANCIANDO UMA JANELA TIPO PANE
-			 
-			 //PEGA O CONTROLE DO CONTROLADOR DE FORMULARIO DE DEPARTAMENTO 
-			 DepartmentFormController controller = loader.getController();
-			
-			 /*ADICIONA NAS CAIXAS DE TEXTO DO FORMULÁRIO OS DADOS DO OBJETO INSTANCIADO.
-			 OBS: O OBJ INSTANCIADO SERVE TANTO PARA ADD UM NOVO OBJ QUANTO PARA ATUALIZAR OS DADOS
-			 DE UM DEP EXISTENTE*/
-			 controller.setEntity(obj);
-			 controller.setDepartmentService(new DepartmentService());
-			 //ADICIONA ESSA CLASSE A LISTA DE OUVINTES, OU SEJA, QUANDO HOUVER MUDANÇAS 
-			 //ESSA CLASSE CHAMA O METODO updateTableView QUE ATUALIZA A LISTA DE DEPARTAMENTOS
-			 //ASSIM QUE ADICIONAR UM NOVO DEPARTAMENTO
-			 controller.subscribeDataChangeListner(this);
-			 controller.updateFormData();
-			 
-			 /*PARA CARREGAR UMA JANELA DE DIALOGO MODAL NA FRENTE DA JANELA EXISTENTE É 
-			  * NECESSÁRIO INSTANCIAR UM NOVO STAGE. ASSIM O RESULTADO SERÁ UMA JANELA SOBRE A OUTRA.
-			  *JANELA MODAL NÃO PERMITE ITERAÇÃO COM OUTRAS JANELAS ENQUANTO A JANELA MODAL NÃO
-			  * FOR FECHADA */
-			 Stage dialogStage = new Stage();
-			 dialogStage.setTitle("Enter department data");
-			 dialogStage.setScene(new Scene(pane));
-			 dialogStage.setResizable(false);//DIZ SE A JANELA PODE OU NÃO SER REDIMENSIONADA
-			 dialogStage.initOwner(parentStage);//DIZ QUAL É A JANELA PAI DA JANELA DIALOGSTAGE 
-			 dialogStage.initModality(Modality.WINDOW_MODAL);// DIZ SE A JANELA É MODAL OU NÃO
-			 dialogStage.showAndWait();
-		}
-		catch(IOException e) {
-			Alerts.showAlert("IO Exception","Error loading view",e.getMessage(), AlertType.ERROR);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();// INSTANCIANDO UMA JANELA TIPO PANE
+
+			// PEGA O CONTROLE DO CONTROLADOR DE FORMULARIO DE DEPARTAMENTO
+			DepartmentFormController controller = loader.getController();
+
+			/*
+			 * ADICIONA NAS CAIXAS DE TEXTO DO FORMULÁRIO OS DADOS DO OBJETO INSTANCIADO.
+			 * OBS: O OBJ INSTANCIADO SERVE TANTO PARA ADD UM NOVO OBJ QUANTO PARA ATUALIZAR
+			 * OS DADOS DE UM DEP EXISTENTE
+			 */
+			controller.setEntity(obj);
+			controller.setDepartmentService(new DepartmentService());
+			// ADICIONA ESSA CLASSE A LISTA DE OUVINTES, OU SEJA, QUANDO HOUVER MUDANÇAS
+			// ESSA CLASSE CHAMA O METODO onDataChange QUE ATUALIZA A LISTA DE DEPARTAMENTOS
+			// ASSIM QUE ADICIONAR UM NOVO DEPARTAMENTO
+			controller.subscribeDataChangeListner(this);
+			controller.updateFormData();
+
+			/*
+			 * PARA CARREGAR UMA JANELA DE DIALOGO MODAL NA FRENTE DA JANELA EXISTENTE É
+			 * NECESSÁRIO INSTANCIAR UM NOVO STAGE. ASSIM O RESULTADO SERÁ UMA JANELA SOBRE
+			 * A OUTRA. JANELA MODAL NÃO PERMITE ITERAÇÃO COM OUTRAS JANELAS ENQUANTO A
+			 * JANELA MODAL NÃO FOR FECHADA
+			 */
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);// DIZ SE A JANELA PODE OU NÃO SER REDIMENSIONADA
+			dialogStage.initOwner(parentStage);// DIZ QUAL É A JANELA PAI DA JANELA DIALOGSTAGE
+			dialogStage.initModality(Modality.WINDOW_MODAL);// DIZ SE A JANELA É MODAL OU NÃO
+			dialogStage.showAndWait();
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
-	@Override //IMPLEMENTAÇÃO DO METODO ABSTRATO DA CLASSE ABSTRATA DataChangeListner.
+	@Override // IMPLEMENTAÇÃO DO METODO ABSTRATO DA CLASSE ABSTRATA DataChangeListner.
 	public void onDataChange() {
 		updateTableView();
 	}
+
+	//CRIA BOTÃO EDIT PARA POSSIBILITAR A EDIÇÃO DOS DEPARTAMENTOS	
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Department obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
+
 }
